4a982aab1959298e21fe226a55c31e3f238bc602
chrisperkinsFL <59574299+chrisperkinsFL@users.noreply.github.com>
2020-07-14 15:55:26 -0400

    quick fix
    Added the shared.scss file back in.

--- dist/js.js
+++ dist/js.js
@@ -1,3 +1,5 @@
+//gets the current selection from the menu and grabs the data from the json file to display the data exmaple.
+
 function selectFunction(){
 	let tv = document.getElementById("select").selectedIndex;
     
@@ -10,8 +12,9 @@ function selectFunction(){
   document.getElementById("tick-value").innerHTML = obj.market[tv].tickValue;
   document.getElementById("point-value").innerHTML = obj.market[tv].pointValue;
   document.getElementById("delivery-months").innerHTML = obj.market[tv].deliveryMonths;
-
 }
+
+//gets the user input values and calaculates the results. 
 
 function addNumbers(){
   let tv = document.getElementById("select").selectedIndex;
@@ -22,7 +25,7 @@ function addNumbers(){
   let exit = parseInt(document.getElementById("exitPrice").value);
   let contracts = parseInt(document.getElementById("numContracts").value);
   let mintick = document.getElementById("min-tick").innerHTML = obj.market[tv].minTick;
-  let tickvalue = document.getElementById("tick-value").innerHTML = obj.market[tv].tickValue;
+  let tickvalue = document.getElementById("tick-value").innerHTML = obj. market[tv].tickValue;
 
   document.getElementById("ticks").innerHTML = (exit - entry) / mintick * contracts;
   document.getElementById("points").innerHTML = (exit - entry) * contracts;
